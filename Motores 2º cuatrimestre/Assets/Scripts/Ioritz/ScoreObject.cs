3d5412d5245270b32c1283f53a099644242a716a
roboioritz <48485367+roboioritz@users.noreply.github.com>
2019-04-16 18:04:43 +0200

    RompecajasV4
    Limpiar, comentar y tooltipear Scripts

--- Motores 2º cuatrimestre/Assets/Scripts/Ioritz/ScoreObject.cs
+++ Motores 2º cuatrimestre/Assets/Scripts/Ioritz/ScoreObject.cs
@@ -6,14 +6,12 @@ public class ScoreObject : MonoBehaviour
 {
 
     private Rigidbody rb;
-
-
-
+       
     // Use this for initialization
     void Start()
     {
-        Destroy(gameObject, 10f);
-        GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-2, 2), 3, Random.Range(-2, 2));
+        Destroy(gameObject, 10f); //El objeto despawneara a los 10 segundos
+        GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-2, 2), Random.Range(2, 4), Random.Range(-2, 2)); //El objeto al espawnear ira en una direccion random
     }
 
     // Update is called once per frame
