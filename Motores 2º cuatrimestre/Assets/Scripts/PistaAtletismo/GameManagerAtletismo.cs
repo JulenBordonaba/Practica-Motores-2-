1e15ec7f5906c9b44142e81af60f5ada5cbd0e83
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-04-30 17:08:41 +0200

    publico prefabs

---
+++ Motores 2º cuatrimestre/Assets/Scripts/PistaAtletismo/GameManagerAtletismo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerAtletismo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void elegirJugadores(Text txtNumero)
+    {
+        int aux = 0;
+        switch (txtNumero.text)
+        {
+            case 2:
+                GameManagerGlobal.i.numeroJugadores = 2;
+                aux = 2;
+                break;
+
+            case 3:
+                GameManagerGlobal.i.numeroJugadores = 3;
+                aux = 3;
+                break;
+
+            case 4:
+                GameManagerGlobal.i.numeroJugadores = 4;
+                aux = 4;
+                break;
+
+            default:
+                break;
+        }
+    }
+}
