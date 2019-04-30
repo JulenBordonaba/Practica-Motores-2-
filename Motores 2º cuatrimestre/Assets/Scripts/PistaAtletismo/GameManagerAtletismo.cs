dc521770bfafea50d935e1faa03e4f7b98910abb
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-04-30 17:21:33 +0200

    codigo comentado

--- Motores 2º cuatrimestre/Assets/Scripts/PistaAtletismo/GameManagerAtletismo.cs
+++ Motores 2º cuatrimestre/Assets/Scripts/PistaAtletismo/GameManagerAtletismo.cs
@@ -5,40 +5,52 @@ using UnityEngine.UI;
 
 public class GameManagerAtletismo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //[Tooltip("Arrastra los botones con el numero para elegir jugadores")]
+    //public List<GameObject> numeros = new List<GameObject>();
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 
-    public void elegirJugadores(Text txtNumero)
-    {
-        int aux = 0;
-        switch (txtNumero.text)
-        {
-            case 2:
-                GameManagerGlobal.i.numeroJugadores = 2;
-                aux = 2;
-                break;
+    //public void elegirJugadores(Text txtNumero)
+    //{
+    //    int aux = 0;
+    //    switch (int.Parse(txtNumero.text))
+    //    {
+    //        case 2:
+    //            GameManagerGlobal.i.numeroJugadores = 2;
+    //            aux = 2;
+    //            break;
 
-            case 3:
-                GameManagerGlobal.i.numeroJugadores = 3;
-                aux = 3;
-                break;
+    //        case 3:
+    //            GameManagerGlobal.i.numeroJugadores = 3;
+    //            aux = 3;
+    //            break;
 
-            case 4:
-                GameManagerGlobal.i.numeroJugadores = 4;
-                aux = 4;
-                break;
+    //        case 4:
+    //            GameManagerGlobal.i.numeroJugadores = 4;
+    //            aux = 4;
+    //            break;
 
-            default:
-                break;
-        }
-    }
+    //        default:
+    //            break;
+    //    }
+
+    //    for (int i = 0; i < aux; i++)
+    //    {
+    //        numeros[i].SetActive(true);
+    //    }
+
+    //    AnimatorClipInfo[] m_CurrentClipInfo;//variable para conseguir el nombre del clip por defecto
+    //    m_CurrentClipInfo = objeto.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0);//ubicacion del clip por defecto
+    ////    objeto.GetComponent<Animator>().Play(m_CurrentClipInfo[0].clip.name);//reproducir el clip por defecto
+    //}
 }
