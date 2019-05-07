22dd8e16561163cfb5c75b19b00e456626b4b65c
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-05-07 17:00:19 +0200

    tiempo laberinto

---
+++ Motores 2º cuatrimestre/Assets/Scripts/Laberinto/GameManagerLaberinto.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerLaberinto : MonoBehaviour
+{
+    public GameObject[] players = new GameObject[4];
+    public bool[] llegados = new bool[4];
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TimeEnd()
+    {
+        print("fin tiempo");
+        for(int i=0;i<G.activePlayers;i++)
+        {
+            if(!llegados[i])
+            {
+                G.positions[i] = G.activePlayers;
+            }
+        }
+        EndGame();
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Podium2");
+    }
+
+
+}
