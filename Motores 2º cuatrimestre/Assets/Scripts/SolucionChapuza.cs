65b9c866ea40e46cfea4f507dbfd2ae1806fc61e
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-05-08 11:42:24 +0200

    arreglado problema de variables repetidas

---
+++ Motores 2º cuatrimestre/Assets/Scripts/SolucionChapuza.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SolucionChapuza : MonoBehaviour
+{
+    //script chapucero para arreglar un problema en el que dos personas hemos usados variables distintas para controlar lo mismo
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        print("active players: " + G.activePlayers);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+
+        G.activePlayers = GameManagerGlobal.i.numeroJugadores;
+        print("active players: " + G.activePlayers);
+    }
+
+
+}
