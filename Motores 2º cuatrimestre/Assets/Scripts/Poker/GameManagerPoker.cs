e6abccfdf918ac14aae5bde07e09a44281ae2c34
juanibar10 <47811666+juanibar10@users.noreply.github.com>
2019-05-21 16:50:14 +0200

    Poker

--- Motores 2º cuatrimestre/Assets/Scripts/Poker/GameManagerPoker.cs
+++ Motores 2º cuatrimestre/Assets/Scripts/Poker/GameManagerPoker.cs
@@ -10,44 +10,132 @@ public class GameManagerPoker : MonoBehaviour
 
     public GameObject[] posiciones;
 
+    public int[] posicion;
+    public int[] carta;
+    public int[] color;
+
     public int seleccionados;
 
-    private int seleccionadorPosicion;
-    private bool imagenesPuestas = false;
+    private string estado = "Colocar Imagenes";
+
+    public bool imagenesPuestas = false;
     
     
     void Update()
     {
-        if(seleccionados >= 4)
+        switch (estado)
         {
-            rondas++;
-            seleccionados = 0;
-            imagenesPuestas = false;
-            for (int i = 0; i <= 3; i++)
-            {
-                jugadores[i].GetComponent<ElegirLadoPoker>().ResetearPosicion();
-                posiciones[i].GetComponent<PuntosSeleccionLado>().ResetearImagenes();
-            }
+            case "Colocar Imagenes":
+
+               
+
+                if (rondas == 1 && !imagenesPuestas)
+                {
+                    for (int i = 0; i <= 3; i++)
+                        jugadores[i].GetComponent<ElegirLadoPoker>().Activo = true;
+                    posicion[0] = RandomNumber(1, posicion);
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (i == posicion[0])
+                            posiciones[posicion[0]].GetComponent<PuntosSeleccionLado>().ActivarImagen(0, 0, true,true);
+                        else
+                            posiciones[i].GetComponent<PuntosSeleccionLado>().ActivarImagen(4, 1, false,false);
+                    }
+                    imagenesPuestas = true;
+                    estado = "Elegir Lados";
+                }
+                else if (rondas == 2 && !imagenesPuestas)
+                {
+                    print("Entra");
+
+                    /*for (int i = 0; i <= 3; i++)
+                        jugadores[i].GetComponent<ElegirLadoPoker>().Activo = true;
+                    */
+                    
+                    estado = "Elegir Lados";
+                }
+
+                break;
+
+            case "Elegir Lados":
                 
+                if (seleccionados >= 4)
+                    estado = "ResetearValores";
+                break;
+                
+            case "ResetearValores":
+                    rondas++;
+                    seleccionados = 0;
+                    imagenesPuestas = false;
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        jugadores[i].GetComponent<ElegirLadoPoker>().ResetearPosicion();
+                        posiciones[i].GetComponent<PuntosSeleccionLado>().ResetearImagenes();
+                    }
+                    for (int i = 0; i < posicion.Length; i++)
+                        posicion[i] = 0;
+                    for (int i = 0; i < color.Length; i++)
+                        color[i] = 0;
+                    for (int i = 0; i < carta.Length; i++)
+                        carta[i] = 0;
 
+
+                estado = "Resetear Posicion";
+                break;
+
+            case "Resetear Posicion":
+                
+                estado = "Colorcar Imagenes";
+                break;
+
+
+            case "Fin de Juego":
+
+                break;
         }
 
-        if(rondas == 1 && !imagenesPuestas)
-        {
-            seleccionadorPosicion = Random.Range(0, 3);
-            print(seleccionadorPosicion);
+        
 
-            for(int i = 0; i <= 3; i++)
-            {
-                if(i == seleccionadorPosicion)
-                    posiciones[seleccionadorPosicion].GetComponent<PuntosSeleccionLado>().ActivarImagen(0, 0, true);
-                else
-                    posiciones[i].GetComponent<PuntosSeleccionLado>().ActivarImagen(4, 1, false);
-            }
-            imagenesPuestas = true;
-        }
+        
     }
 
+    public int RandomNumber(int caso,int[] array)
+    {
+        switch (caso)
+        {
+            case 1:
+                int number = Random.Range(0, 3);
+                return number;
+            case 2:
+                int number2 = Random.Range(0, 3);
+                    do
+                    {
+                        if (number2 == array[0])
+                            number2 = Random.Range(0, 3);
+                    }
+                    while (number2 == array[0]);
+                return number2;
+            case 3:
+                int number3 = Random.Range(0, 3);
+                do
+                {
+                    if (number3 == array[0])
+                        number3 = Random.Range(0, 3);
+                }
+                while (number3 == array[0] || number3 == array[1]);
+                return number3;
+            case 4:
+                int number4 = Random.Range(0, 3);
+                do
+                {
+                    if (number4 == array[0])
+                        number4 = Random.Range(0, 3);
+                }
+                while (number4 == array[0] || number4 == array[1] || number4 == array[2]);
+                return number4;
+        }
+        return 0;
+    }
 
 
     public void sumarSeleccionados()
