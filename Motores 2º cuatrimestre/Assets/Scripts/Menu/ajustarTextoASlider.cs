0257edcfddf1ea4135749aaa9e33a4739539e34e
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-06-04 15:58:42 +0200

    Haciendo menus

---
+++ Motores 2º cuatrimestre/Assets/Scripts/Menu/ajustarTextoASlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class ajustarTextoASlider : MonoBehaviour
+{
+
+    public Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int aux;
+        aux = (int)slider.value;
+        gameObject.GetComponent<Text>().text = aux.ToString();
+    }
+}
