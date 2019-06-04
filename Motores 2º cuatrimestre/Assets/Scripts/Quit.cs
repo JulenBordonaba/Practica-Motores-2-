1cc355832b63705a520658c3803840a78ebd3c75
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-06-05 01:52:08 +0200

    trabajando en menus

---
+++ Motores 2º cuatrimestre/Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    //función para cerrar el juego
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
